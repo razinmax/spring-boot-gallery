b4308786eb05eb90a96faeb3df297626c8a6ab6f
Max Razin <razinmax02@gmail.com>
2025-10-15 20:50:30 +0500

    added bootstrap and creationDate for Post

--- src/main/java/com/example/demo/service/PostService.java
+++ src/main/java/com/example/demo/service/PostService.java
@@ -3,15 +3,16 @@ package com.example.demo.service;
 import com.example.demo.model.Post;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 
 @Service
 public class PostService {
     public List<Post> listAllPosts(){
         return List.of(
-                new Post("Hello world!"),
-                new Post("Wassup everybody?"),
-                new Post("Does anyone see these posts?")
+                new Post("Hello world!",new Date()),
+                new Post("Wassup everybody?", new Date(System.currentTimeMillis()-86400000L)),
+                new Post("Does anyone see these posts?", new Date(System.currentTimeMillis()-2*86400000L))
         );
     }
 }
